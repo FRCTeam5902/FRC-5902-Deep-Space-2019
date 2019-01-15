38d48f5b61ee5872b17b3e61d3d3f2688bdc4690
FRCTeam5902 <frc5902@newheightsonline.org>
2019-01-15 16:33:55 -0500

    RobotMap copied from last year (only the TalonSRX controllers copied)

--- ROBOT_PROJECT_2019/src/main/java/frc/robot/RobotMap.java
+++ ROBOT_PROJECT_2019/src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,22 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.AnalogAccelerometer;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -14,13 +30,37 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+    public static AnalogAccelerometer sensorBaseAAccelerometer;
+    public static WPI_TalonSRX driveTrainleftDriveLead;
+    public static WPI_TalonSRX driveTrainrightDriveLead;
+    public static WPI_TalonSRX driveTrainleftDriveFollow;
+    public static WPI_TalonSRX driveTrainrightDriveFollow;
+    public static SpeedController elevatorUpDown;
+    public static SpeedController wheelA;
+    public static SpeedController wheelB;
+    public static SpeedController pole;
+    public static SpeedController lights;
+    public static DifferentialDrive driveTrainrobotDrive;
+    public static Servo cameraControlpanServo;
+    public static DigitalOutput limSwitch;
+    public static void init() {
+        sensorBaseAAccelerometer = new AnalogAccelerometer(0);
+        sensorBaseAAccelerometer.setSensitivity(0.0);
+        sensorBaseAAccelerometer.setZero(2.5);
+        
+        //Create Left Motors
+        driveTrainleftDriveLead = new WPI_TalonSRX(1);
+        driveTrainleftDriveFollow = new WPI_TalonSRX(2);
+        
+        //Create Right Motors
+        driveTrainrightDriveLead = new WPI_TalonSRX(3);
+        driveTrainrightDriveFollow = new WPI_TalonSRX(4);
+        
+        // Create Drive Train
+        driveTrainrobotDrive = new DifferentialDrive(driveTrainleftDriveLead, driveTrainrightDriveLead);
+        driveTrainrobotDrive.setSafetyEnabled(true);
+        driveTrainrobotDrive.setExpiration(0.1);
+      driveTrainrobotDrive.setMaxOutput(1.0);
+    }
+  }
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
-}
