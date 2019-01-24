f9ee675a72b73fa02e19d132f29542edf9d0c23d
SagesOfRPG <wayne@sagesofrpg.com>
2019-01-23 21:58:05 -0500

    Edits to Hacth Commands

--- ROBOT_PROJECT_2019/src/main/java/frc/robot/commands/hatchServoCenter.java
+++ ROBOT_PROJECT_2019/src/main/java/frc/robot/commands/hatchServoCenter.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import frc.robot.Robot;
-import frc.robot.subsystems.hatchSystem;
 
 public class hatchServoCenter extends Command {
   public hatchServoCenter() {
