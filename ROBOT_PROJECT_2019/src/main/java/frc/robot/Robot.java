497f75316d5ea44eb739094d396de41fd7d01c62
FRCTeam5902 <frc5902@newheightsonline.org>
2019-01-26 10:38:09 -0500

    Removed old camera code

--- ROBOT_PROJECT_2019/src/main/java/frc/robot/Robot.java
+++ ROBOT_PROJECT_2019/src/main/java/frc/robot/Robot.java
@@ -47,26 +47,6 @@ public class Robot extends TimedRobot {
     //m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     //SmartDashboard.putData("Auto mode", m_chooser);
-
-    //newcamera Code 2019
-    new Thread(() -> {
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-      camera.setResolution(640, 480);
-      
-      CvSink cvSink = CameraServer.getInstance().getVideo();
-      CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-      
-      Mat source = new Mat();
-      Mat output = new Mat();
-      
-      while(!Thread.interrupted()) {
-          cvSink.grabFrame(source);
-          Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-          outputStream.putFrame(output);
-      }
-  }).start();
-
-  //END 2019 Camera Code
 }
 
   /**
