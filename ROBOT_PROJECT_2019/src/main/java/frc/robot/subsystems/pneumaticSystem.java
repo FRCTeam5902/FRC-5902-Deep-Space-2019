bf8513cc3a60e2aa607c1a7bd8dce7353df2a2f2
SagesOfRPG <wayne@sagesofrpg.com>
2019-01-30 17:35:20 -0500

    Added Default Command to Pneumatic Subsystem

--- ROBOT_PROJECT_2019/src/main/java/frc/robot/subsystems/pneumaticSystem.java
+++ ROBOT_PROJECT_2019/src/main/java/frc/robot/subsystems/pneumaticSystem.java
@@ -39,6 +39,7 @@ public class  pneumaticSystem extends Subsystem {
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new solenoidDoNothing());
   }
 
 
