6d8dce40eb06468d5be7202aef4abd5eb6b84f2b
FRCTeam5902 <FRC5902@newheightsonline.org>
2019-02-08 15:26:44 -0500

    Added Ramp Up Speed

--- ROBOT_PROJECT_2019/src/main/java/frc/robot/commands/arcadeDrive.java
+++ ROBOT_PROJECT_2019/src/main/java/frc/robot/commands/arcadeDrive.java
@@ -6,6 +6,10 @@ import frc.robot.Robot;
 
 public class arcadeDrive extends Command {
 
+  
+
+   
+
     public arcadeDrive() {
     	requires(Robot.driveTrain);
     }
@@ -13,45 +17,59 @@ public class arcadeDrive extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() { 
-    	
+    //Ramp up Speed
+
     }
+        public double RSpeedZ;
+        public double RSpeedY;
+    
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
         double driveSpeed = 1;
         //forward and backward sensitivity, y value of joystick
         double driveSensitivity = .5;
+        
         //turning sensitivity, z value of joystick
         double turnSensitivity = .5;
         double gety = Robot.oi.getlogitechJoy().getY();
         double getz = Robot.oi.getlogitechJoy().getZ();
-  
-        SmartDashboard.putNumber("Robot.speed", Robot.speed);
+        for (double IY = 100; IY > 0; IY--) {
+            RSpeedY = (gety / IY);
+        }
+        for (double IZ = 100; IZ > 0; IZ--) {
+            RSpeedZ = (gety / IZ);
+        }
+
         Robot.speed = (driveSpeed);
-        //added minimum getz and gety so that small adjustments don't power the motors
+        SmartDashboard.putNumber("Robot.speed", Robot.speed);
+        // added minimum getz and gety so that small adjustments don't power the motors
         if (getz < .2 && getz > -.2)
         {
             if(gety<-.1)
             {
-                Robot.driveTrain.arcadeDrive(1.5*-(driveSensitivity)*gety, 0, Robot.speed);
+                Robot.driveTrain.arcadeDrive(1.5*-(driveSensitivity)*RSpeedY, 0, Robot.speed);
             }
             else
             {
-                Robot.driveTrain.arcadeDrive(-(driveSensitivity)*gety, 0, Robot.speed);
+                Robot.driveTrain.arcadeDrive(-(driveSensitivity)*RSpeedY, 0, Robot.speed);
             }
         }
         else
         {
-            if(gety<-.1)
+            if (gety < -.1)
             {
-                Robot.driveTrain.arcadeDrive(1.5*-(driveSensitivity)*gety, getz*(turnSensitivity/driveSensitivity), Robot.speed);
+                Robot.driveTrain.arcadeDrive(1.5*-(driveSensitivity)*RSpeedY, RSpeedZ*(turnSensitivity/driveSensitivity), Robot.speed);
             }
             else
             {
-                Robot.driveTrain.arcadeDrive(-(driveSensitivity)*gety, getz*(turnSensitivity/driveSensitivity), Robot.speed);
+                Robot.driveTrain.arcadeDrive(-(driveSensitivity)*RSpeedY, RSpeedZ*(turnSensitivity/driveSensitivity), Robot.speed);
             }
         }
     }
+
+    
+    
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
